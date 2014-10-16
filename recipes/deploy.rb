5d03cce2e8bad24a11fdb1426f11a6a299c06480
Alexander Zaytsev <alexander@say26.com>
2014-10-16 14:40:58 +0600

    Add sleep

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -31,7 +31,6 @@ node[:deploy].each do |application, deploy|
 
   Chef::Log.debug("Restarting Sidekiq Application: #{application}")
   execute "restart Rails app #{application}" do
-    command node[:sidekiq][application][:restart_command]
+    command "sleep 60 && #{node[:sidekiq][application][:restart_command]}"
   end
-
 end
